b8fd0c2dca2745912bb3166d290d45521617d63b
dm0123 <dm0123@gmail.com>
2014-11-29 21:16:13 +0300

    и еще чуть-чуть

--- core/src/com/mygdx/sparcade/model/Enemy.java
+++ core/src/com/mygdx/sparcade/model/Enemy.java
@@ -1,24 +1,18 @@
 package com.mygdx.sparcade.model;
 
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by Дмитрий on 27.11.2014.
  */
-public class Enemy {
-    static final int SIZE = 64;
-    public enum State { LIVE, DEAD } // тут нужно подумать еще
-    public State state = State.LIVE;
-    public Rectangle bounds = new Rectangle();
+public class Enemy  extends Starship{
     public String filename = new String("zloy_kot.png");
     public int speed = 1;
     public int angle = 360; // 360=0 - Вверх, 90 - вправо, 180 - вниз
-    public int size = 64;
 
 
     public Enemy(Vector2 position)
     {
-        bounds = new Rectangle(position.x, position.y, SIZE, SIZE);
+       super(position);
     }
 }
