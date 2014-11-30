a943f806c27019afee58d66397c3323d19686c26
GranoDeAvena <iamhereforyou@mail.ru>
2014-11-30 23:44:49 +0300

    небольшой костыль на управление кораблем

--- core/src/com/mygdx/sparcade/GameScreen.java
+++ core/src/com/mygdx/sparcade/GameScreen.java
@@ -32,7 +32,7 @@ public class GameScreen implements Screen
 
         // create starship
         enemy = new Enemy(new Vector2(800/2-64/2, 200));
-        ship = new Starship(new Vector2(0, 480/2 - 50));
+        ship = new Starship(new Vector2(800 - 170 - 10, 480/2 - 50));
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
@@ -59,21 +59,46 @@ public class GameScreen implements Screen
         game.batch.draw(fon, 0, 0);
         game.font.draw(game.batch, "Enemy class test, bounds: x=" + (enemy.bounds.x) + ", y= " + (enemy.bounds.y)+", enemy speed= " + (enemy.speed)+" enemy state: "+enemy.state, 0, 470);
         game.batch.draw(enemypic, enemy.bounds.x, enemy.bounds.y);
-        if (Gdx.input.isTouched()) {       // Управление кораблем, не хорошо что весь код в  одной куче
-            Vector3 position = new Vector3();
-            position.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(position);
-            if ((int) (position.x) < 200 && ((int) (position.y) < 200)) {
+
+
+        if (Gdx.input.isTouched() || Gdx.input.isTouched(1)) {       // Управление кораблем, не хорошо что весь код в  одной куче
+            Vector3 touch = new Vector3();
+            Vector3 touch2 = new Vector3();
+            touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touch);
+            game.font.draw(game.batch, "Touch 1 x - "+Gdx.input.getX()+"Touch 1 y - "+Gdx.input.getX(), 0, 80);
+            if (Gdx.input.isTouched(1)) {
+                touch2.set(Gdx.input.getX(1), Gdx.input.getY(1), 0);
+                camera.unproject(touch2);
+                game.font.draw(game.batch, "Touch 2 x - "+Gdx.input.getX(1)+"Touch 2 y - "+Gdx.input.getX(1), 0, 20);
+                if (touch2.x > 600 && touch.x > 600)
+                    touch = touch2;
+                //else
+                //if (touch2.x > 400)
+
+            }
+
+            if ((int) (touch.x) > 600 && ((int) (touch.y) < 200)) {  // Влево
                 ship.bounds.y -= 2;
                 game.batch.draw(shipPicLeft, ship.bounds.x, ship.bounds.y);
             }
             else
-                if ((int) (position.x) < 200 && ((int) (position.y) > 200)) {
-                    ship.bounds.y += 2;
-                    game.batch.draw(shipPicRight, ship.bounds.x, ship.bounds.y);
-                }
-                else
-                    game.batch.draw(shipPic, ship.bounds.x, ship.bounds.y);
+            if ((int) (touch.x) > 600 && ((int) (touch.y) > 200)) {  // Вправо
+                ship.bounds.y += 2;
+                game.batch.draw(shipPicRight, ship.bounds.x, ship.bounds.y);
+            }
+            else
+            if ((int) (touch.x) > 600 && ((int) (touch.y) < 200)) {
+                ship.bounds.y -= 2;
+                game.batch.draw(shipPicLeft, ship.bounds.x, ship.bounds.y);
+            }
+            else
+            if ((int) (touch.x) > 600 && ((int) (touch.y) > 200)) {
+                ship.bounds.y += 2;
+                game.batch.draw(shipPicRight, ship.bounds.x, ship.bounds.y);
+            }
+            else
+                game.batch.draw(shipPic, ship.bounds.x, ship.bounds.y);
         }
         else
             game.batch.draw(shipPic, ship.bounds.x, ship.bounds.y);
